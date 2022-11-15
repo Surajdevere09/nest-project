3585b97c7a5a6f8f8a98c5e24f3f407e7ff93145
Suraj <suraj.devere@vitra.ai>
2022-11-15 18:24:48 +0530

    new branch

--- src/chai/chai.module.ts
+++ src/chai/chai.module.ts
@@ -1,6 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ChaiController } from './chai.controller';
 import { ChaiService } from './chai.service';
+import { chai } from './entities/chai.entity';
 
-@Module({ controllers: [ChaiController], providers: [ChaiService] })
+@Module({
+  imports: [TypeOrmModule.forFeature([chai])],
+  controllers: [ChaiController],
+  providers: [ChaiService],
+})
 export class ChaiModule {}
--- src/chai/entities/chai.entity.ts
+++ src/chai/entities/chai.entity.ts
@@ -1,6 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity() //sql table === 'chai'
 export class chai {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   brand: string;
+
+  @Column('json', { nullable: true })
   flavors: string[];
 }
