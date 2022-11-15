8ba29068fa8714e1fa39ce35162065121786cfad
Suraj <suraj.devere@vitra.ai>
2022-11-15 17:43:28 +0530

    latest changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesController } from './coffees/coffees.controller';
 import { ChaiModule } from './chai/chai.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [ChaiModule],
+  imports: [
+    ChaiModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'password',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController, CoffeesController],
   providers: [AppService],
 })
